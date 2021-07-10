f11100e181e71f8349a045a4427c5b9f1f2b2efb
cgcarlosg <cgcarlosg@hotmail.com>
2021-07-09 21:23:42 -0500

    Add States

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import calculate from '../logic/calculate';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 function App() {
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = buttonName => {
+    const result = calculate(calculation, buttonName);
+    setCalculation(result);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Display />
-        <ButtonPanel />
+        <Display result={calculation} />
+        <ButtonPanel clickHandler={handleClick} />
       </header>
     </div>
   );
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,40 +3,37 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   let total;
   let result;
+  const one = Big(numberOne);
+  const two = Big(numberTwo);
+
 
   if (operation === '+') {
-    total = Big(numberOne).plus(numberTwo).toString();
+    total = one.plus(two).toString();
     result = { total: null, next: total, operation };
   }
 
   if (operation === '-') {
-    total = Big(numberOne).minus(numberTwo).toString();
+    total = one.minus(two).toString();
     result = { total: null, next: total, operation };
   }
 
   if (operation === '÷') {
-    if (numberTwo === '0') {
+    if (two === '0') {
       result = { total: 'Math Error', next: null, operation };
     } else {
-      total = Big(numberOne).div(numberTwo).toString();
+      total = one.div(two).toString();
       result = { total: null, next: total, operation };
     }
   }
 
   if (operation === 'X') {
-    total = Big(numberOne).times(numberTwo).toString();
+    total = one.times(two).toString();
     result = { total: null, next: total, operation };
   }
 
   if (operation === '%') {
-    if (numberOne === null) {
-      total = Big(numberOne).div(100).toString();
-      result = { total, next: null, operation };
-    }
-    if (numberTwo === null && numberOne != null) {
-      total = Big(numberTwo).div(100).toString();
-      result = { total, next: null, operation };
-    }
+    total = one.mod(two).toString();
+    result = { total: null, next: total, operation };
   }
 
   return result;
