6f8e3aba7570b54879cae1f89b839a62ea4df887
cgcarlosg <cgcarlosg@hotmail.com>
2021-07-13 06:50:34 -0500

    Add tests and navbar-home styles

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => (
+  <div className="home-body">
+    <h1>
+      Welcome to My Calculator!
+      {' '}
+    </h1>
+    <p>
+      Hi!
+      {' '}
+      This calculator has been done as a project to learn more about how React.js works. You can go ahead and
+      {' '}
+      try it out!
+    </p>
+    <p>
+      This is the source code:
+      {' '}
+      <a href="https://github.com/cgcarlosg/app-calculator1978" target="_blank" rel="noreferrer">Github Calculator</a>
+    </p>
+  </div>
+);
+
+export default Home;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
+import Home from './Home';
+import Navbar from './Navbar';
+import Quote from './Quote';
 
 const Routes = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
-      <Route path="/" component={App} />
+      <Route path="/" component={Home} exact />
+      <Route path="/calculator" component={App} />
+      <Route path="/quote" component={Quote} />
     </Switch>
   </BrowserRouter>
 );
